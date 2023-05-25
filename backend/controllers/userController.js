6c1769fbf13d65115275138fafc548ccadd3a911
Murshid Bin Ibrahim <murshidshazz@gmail.com>
2023-05-25 20:57:47 +0530

    User Creation ~ hashPassword using bcrypt - async/await way

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,37 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+  const saltRounds = 10; //bcrypt applies a salt — a unique random string that makes the hash unpredictable
+
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    //console.log(hash);
+    return hash;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 const user_create_get = (req, res) => {
   res.status(200).json({ msg: "GET-USER REGISTRATION" });
 };
 
-const user_create_post = (req, res) => {
-  res.status(200).json({ msg: "POST-USER REGISTRATION" });
+const user_create_post = async (req, res) => {
+  const { username, password, address } = req.body;
+  const hashedPassword = await hashPassword(password);
+  console.log("hashedPassword " + hashedPassword);
+  try {
+    const user = await User.create({
+      username: username,
+      password: hashedPassword,
+      address: address,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const user_update = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 const express = require("express");
+const app = express();
 require("dotenv").config();
 
-const app = express();
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //If Connection is Successfull , then start the server
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT}/`);
+    });
+
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection failed - " + err);
+  });
+
+//middleware
+//express.json() parses incoming JSON requests and puts the parsed data in req.body.
+app.use(express.json());
 
 //importing routers
 const userRouter = require("./routes/user-route");
 
 //configure route
 app.use("/api/users", userRouter);
-
-//express.json() parses incoming JSON requests and puts the parsed data in req.body.
-app.use(express.json());
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
-});
